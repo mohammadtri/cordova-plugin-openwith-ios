fb1e2ffe8356c4b11e86d8ab016b5d68665e9fca
mohammadtri <45417939+mohammadtri@users.noreply.github.com>
2019-06-17 08:27:58 +0700

    Update iosAddTarget.js
    Change requireCordovaModule to require

--- hooks/iosAddTarget.js
+++ hooks/iosAddTarget.js
@@ -34,6 +34,7 @@ const BUNDLE_SUFFIX = '.shareextension';
 
 var fs = require('fs');
 var path = require('path');
+var xcode = require("xcode");
 
 function redError(message) {
     return new Error('"' + PLUGIN_ID + '" \x1b[1m\x1b[31m' + message + '\x1b[0m');
@@ -113,7 +114,7 @@ function getCordovaParameter(configXml, variableName) {
 // }
 
 function parsePbxProject(context, pbxProjectPath) {
-  var xcode = context.requireCordovaModule('xcode');
+//   var xcode = context.requireCordovaModule('xcode');
   console.log('    Parsing existing project at location: ' + pbxProjectPath + '...');
   var pbxProject;
   if (context.opts.cordova.project) {
@@ -205,7 +206,7 @@ console.log('Adding target "' + PLUGIN_ID + '/ShareExtension" to XCode project')
 
 module.exports = function (context) {
 
-  var Q = context.requireCordovaModule('q');
+  var Q = require('q');
   var deferral = new Q.defer();
 
   // if (context.opts.cordova.platforms.indexOf('ios') < 0) {
